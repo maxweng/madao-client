aa54804e491ccab58b18178dc189517d58faa5cb
zhengz <mljsgto222@gmail.com>
2016-11-23 13:48:31 +0800

    update Ether

--- app/main/resources/Ether.js
+++ app/main/resources/Ether.js
@@ -3,8 +3,32 @@ ionicApp
 .factory('Ether', ['$resource', '$q',
 function(){
     var Ether = $resource('/api', {}, {
+        //balance: addr
+		//isClassic: true
         getBalance: {
             method: 'POST'
+        },
+        //txdata: addr
+		//isClassic: true
+        getTransactionData: {
+            method: 'POST'
+        },
+        //rawtx: rawTx,
+		//isClassic: true
+        sendRawTx: {
+            method: 'POST'
+        },
+        // estimatedGas: txobj,
+		// isClassic: true
+        getEstimatedGas: {
+            method: 'POST'
+        },
+        // ethCall: txobj,
+		// isClassic: true
+        getEthCall: {
+            method: 'POST'
         }
-    })
+    });
+
+    return Ether;
 }]);
